6a10e7f4d8da6680006a80ec81bb5b8d497246a7
AmiltoJ <Amiltosmaniotto@gmail.com>
2023-05-07 16:56:09 -0300

    commit segundo

--- src/components/user/CardPerson.jsx
+++ src/components/user/CardPerson.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import stylePerson from "./CardPerson.module.css";
 
 
-export function CardPerson({ cover, avatar, name, numero }) {
+export function CardPerson({ cover, avatar, name, numero, stacks }) {
   return (
     <div className={stylePerson.card}>
       <img src={cover} alt="Cover" />
