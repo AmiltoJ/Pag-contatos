e6af14989740c298157a5701a3ce1a802fcd0e4e
AmiltoJ <Amiltosmaniotto@gmail.com>
2023-05-09 22:23:42 -0300

    commit 7

--- src/components/user/CardPerson.jsx
+++ src/components/user/CardPerson.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-
 import stylePerson from "./CardPerson.module.css";
 
 
-export function CardPerson({ cover, avatar, name, office, stacks }) {
-  return (
-    <div className={stylePerson.card}>
-      <img src={cover} alt="Cover" />
-      <img className={stylePerson.avatar} src={avatar} alt="Avatar" />
-      <p className={stylePerson.p1}>{name}</p>
-      <p className={stylePerson.p2}>{office}</p>
-    </div>
-  );
-}
+  export function CardPerson({ cover, avatar, name, office, stacks }) {
+    return (
+      <div className={stylePerson.card}>
+        <img src={cover} alt="Cover" />
+        <i mg className={stylePerson.avatar} src={avatar} alt="Avatar" />
+        <p className={stylePerson.p1}>{name}</p>
+        <p className={stylePerson.p2}>{office}</p>
+        <span className={stylePerson.p3}>#CSS</span>
+        <span>#React-Native</span>
+        <span>#JavaScript</span>
+        <span>#C++</span>
+      </div>
+    );
+  }
